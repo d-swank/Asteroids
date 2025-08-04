458dfa65f7a866632f14d49a695ec8aed708499b
d-swank <dakotaswank18@gmail.com>
2025-08-04 16:32:17 -0700

    add Group class to hold and  manage multiple objects

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ from constants import *
 from player import Player
 
 def main():
-    pygame.init() # Initialize Pygame
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Set the screen size
-    clock = pygame.time.Clock() # Create a clock object
-    dt = 0  # Delta time for frame rate control
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Create a player instance
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    # Main game loop
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        screen.fill("black")  # Fill the screen with black
-        player.draw(screen)
-        player.update(dt)
-        pygame.display.flip()  # Update the display
 
-        dt = clock.tick(60) / 1000.0  # limit the framerate to 60 FPS
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
