a321257d754bbbcb5ee60cf884ed67c5719d590a
d-swank <dakotaswank18@gmail.com>
2025-08-04 15:50:01 -0700

    add  player triangle with a hitbox of a circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init() # Initialize Pygame
@@ -7,13 +8,16 @@ def main():
     clock = pygame.time.Clock() # Create a clock object
     dt = 0  # Delta time for frame rate control
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Create a player instance
+
     # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        screen.fill((0, 0, 0))  # Fill the screen with black
+        screen.fill("black")  # Fill the screen with black
+        player.draw(screen)
         pygame.display.flip()  # Update the display
 
         dt = clock.tick(60) / 1000.0  # limit the framerate to 60 FPS
